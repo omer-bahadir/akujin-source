12807d52fc295639ae34eae8dadf74906dc47b76
omer <omerbahadir2006@gmail.com>
2021-05-07 01:19:39 -0700

    Update rotmgprod.sql

--- server-source/db/rotmgprod.sql
+++ server-source/db/rotmgprod.sql
@@ -7,8 +7,8 @@
 -- Server Version: 5.5.16
 -- PHP-Version: 5.3.8
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
